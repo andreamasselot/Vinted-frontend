2288aab2214c8eeae16a1840ef6d78daa0c7447c
Andréa Masselot <andrea.nicolas128@gmail.com>
2022-11-10 18:15:48 +0100

    remove token

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,37 @@
 import logo from "../assets/img/vintedlogo.png";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
 const Header = () => {
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const newToken = Cookies.get("token");
+    setToken(newToken);
+  }, []);
   return (
     <>
       <header>
         <img src={logo} alt="logo" />
 
         <div className="button-1">
-          <Link to={"/signup"}>S'inscrire</Link>
-          <Link to={"/login"}>Se connecter</Link>
+          {token ? (
+            <button
+              className="deco"
+              onClick={() => {
+                Cookies.remove("token");
+                setToken("");
+              }}
+            >
+              Se déconnecter
+            </button>
+          ) : (
+            <div>
+              <Link to={"/signup"}>S'inscrire</Link>
+              <Link to={"/login"}>Se connecter</Link>
+            </div>
+          )}
+
           <button className="button-2">Vends tes articles</button>
         </div>
       </header>
