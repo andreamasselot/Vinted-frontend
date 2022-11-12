99dea6ac948891e653152e3585736df8be1d5d0a
Andréa Masselot <andrea.nicolas128@gmail.com>
2022-11-12 18:46:35 +0100

    component added

---
+++ src/components/Card.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+const Card = (props) => {
+  return (
+    <Link to={`/offers/${props.id}`}>
+      <div className="offers-container" key={props.id}>
+        {props.elem.owner ? (
+          <div className="users">
+            <img
+              className="avatars"
+              src={props.elem.owner.account.avatar.secure_url}
+              alt="avatar"
+            />
+            <p> {props.elem.owner.account.username}</p>
+          </div>
+        ) : (
+          <p className="empty-div"></p>
+        )}
+
+        <img src={props.elem.product_image.secure_url} alt="phot" />
+        <p>{props.elem.product_price} €</p>
+        <p>{props.elem.product_details[1].TAILLE}</p>
+        <p>{props.elem.product_details[0].MARQUE}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Card from "../components/Card";
 const Home = () => {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
@@ -28,31 +28,8 @@ const Home = () => {
       <div className="banner"></div>
       <section className="container offers-section">
         {data.offers.map((elem) => {
-          //console.log(elem._id);
           const id = elem._id;
-          return (
-            <Link to={`/offers/${id}`}>
-              <div className="offers-container" key={id}>
-                {elem.owner ? (
-                  <div className="users">
-                    <img
-                      className="avatars"
-                      src={elem.owner.account.avatar.secure_url}
-                      alt="avatar"
-                    />
-                    <p> {elem.owner.account.username}</p>
-                  </div>
-                ) : (
-                  <p className="empty-div"></p>
-                )}
-
-                <img src={elem.product_image.secure_url} alt="phot" />
-                <p>{elem.product_price} €</p>
-                <p>{elem.product_details[1].TAILLE}</p>
-                <p>{elem.product_details[0].MARQUE}</p>
-              </div>
-            </Link>
-          );
+          return <Card id={elem._id} elem={elem} key={id} />;
         })}
       </section>
     </>
